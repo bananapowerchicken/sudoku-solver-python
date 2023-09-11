3a260285ac6199f039d4d172f248ff596f648e6c
Анна МительковаÐ <221882@mail.ru>
2023-09-11 15:56:35 +0300

    saving img with current sudoku to read it later

---
+++ read_from_screen_solver.py
@@ -0,0 +1,32 @@
+import pyautogui as pg
+import time
+import os
+
+# # пробую наведение мыши
+# print(pg.position())
+print(pg.size())
+
+time.sleep(2)
+print(pg.position())
+
+
+# pg.mouseDown(button='right')
+pg.click()  # клик ЛКМ работает
+pg.rightClick() # клик ПКМ работает
+pg.hotkey('down')
+pg.hotkey('enter')
+
+# теперь нужно названия файла ввести - как-то он странно вводится - теряет постепенно первые буквы
+time.sleep(2)
+name = 'puzzle'
+pg.typewrite(name, 0.5)
+pg.hotkey('enter')
+
+
+# тут должно быть распознавание цифр с сохраненной картинки
+
+
+
+# для простоты удаляю судоку после решения
+time.sleep(10)
+os.remove(f'{name}.png')
