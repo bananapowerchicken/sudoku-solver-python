f169a1144d8c81217b2abc5948ca54af7cf34e86
Анна МительковаÐ <221882@mail.ru>
2023-09-21 23:06:42 +0300

    partly fixed main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# сохранение картинки
-# считывание
-# решение
 import time
 import os
 from utils import download_img, get_data, solve, download_img_to_certain_folder
@@ -18,18 +15,12 @@ grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
-# download_img()
-# time.sleep(10)
-# get_data(grid)
-# time.sleep(1)
-# solve(grid)
-
-# os.remove('puzzle.png')
-
-# D:\code\sudoku-solver-python - путь нужной мне папки
+# D:\code\sudoku-solver-python - needed path to the curr folder
 adress = 'D:\code\sudoku-solver-python'
 download_img_to_certain_folder(adress)
 time.sleep(10)
 get_data(grid)
 time.sleep(1)
 solve(grid)
+
+os.remove('puzzle.png')
